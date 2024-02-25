82900e3822ca27621c81f1783b1e03aa59f4df28
nimishg5 <nimishgupta1992@yahoo.in>
2024-02-26 01:12:29 +0530

    fixes in some files

--- emailSender.py
+++ emailSender.py
@@ -1,16 +1,16 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
-from email.mime.text import MIMEText 
+from email.mime.text import MIMEText
 from email import encoders
 import appConstants
-  
+
 def trigger_mail(analysis):
     message = 'Subject: {}\n\n{}'.format(appConstants.MAIL_SUBJECT, analysis)
 
-    s = smtplib.SMTP(appConstants.GMAIL_SPTP_URL, appConstants.GMAIL_SMTP_PORT) 
+    s = smtplib.SMTP(appConstants.GMAIL_SPTP_URL, appConstants.GMAIL_SMTP_PORT)
     s.ehlo()
-    s.starttls() 
+    s.starttls()
     s.login(appConstants.GMAIL_SMTP_LOGIN_ID, appConstants.GMAIL_SMTP_APP_PASSWORD)
     s.sendmail(appConstants.GMAIL_SMTP_LOGIN_ID, appConstants.TO_MAILING_LIST, message)
     # print('message is ' + message)
@@ -20,58 +20,58 @@ def trigger_mail(analysis):
 def trigger_mail_2(analysis, file):
     fromaddr = appConstants.GMAIL_SMTP_LOGIN_ID
     toaddr = appConstants.TO_MAILING_LIST
-    
-    # instance of MIMEMultipart 
-    msg = MIMEMultipart() 
-    
-    # storing the senders email address   
-    msg['From'] = fromaddr 
-    
-    # storing the receivers email address  
-    msg['To'] = toaddr 
-    
-    # storing the subject  
+
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = ", ".join(toaddr)
+
+    # storing the subject
     msg['Subject'] = appConstants.MAIL_SUBJECT
-    
-    # string to store the body of the mail 
+
+    # string to store the body of the mail
     body = analysis
-    
-    # attach the body with the msg instance 
-    msg.attach(MIMEText(body, 'plain')) 
-    
-    # open the file to be sent  
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
     filename = file
-    attachment = open(file, "rb") 
-    
-    # instance of MIMEBase and named as p 
-    p = MIMEBase('application', 'octet-stream') 
-    
-    # To change the payload into encoded form 
-    p.set_payload((attachment).read()) 
-    
-    # encode into base64 
-    encoders.encode_base64(p) 
-    
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
-    
-    # attach the instance 'p' to instance 'msg' 
-    msg.attach(p) 
-    
-    # creates SMTP session 
-    s = smtplib.SMTP('smtp.gmail.com', 587) 
-    
-    # start TLS for security 
-    s.starttls() 
-    
-    # Authentication 
-    s.login(fromaddr, appConstants.GMAIL_SMTP_APP_PASSWORD) 
-    
-    # Converts the Multipart msg into a string 
-    text = msg.as_string() 
-    
-    # sending the mail 
-    s.sendmail(fromaddr, toaddr, text) 
-    
-    # terminating the session 
-    s.quit() 
+    attachment = open(file, "rb")
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(fromaddr, appConstants.GMAIL_SMTP_APP_PASSWORD)
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail \
+    s.sendmail(fromaddr, toaddr, text)
+
+    # terminating the session
+    s.quit()
     print(appConstants.MAIL_SEDNING_SUCCESS_MESSAGE)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import stockAnalyzerForNiftyFifty
 import DividendStocksScrapper
 import ExcelWriter
-from emailSender import *
+from emailSender import trigger_mail_2
 from emailMessageResultModel import *
 
 print("Starting our Python Code")
+file_name = 'DividendStocksFiltered_nimish.xlsx'
 df = DividendStocksScrapper.driver_func(10, 250)
-ExcelWriter.write_into_excel(df, "DividendStocksFiltered_nimish.xlsx")
+ExcelWriter.write_into_excel(df, file_name)
 stockAnalyzerForNiftyFifty.triggerFirstCalculation()
 
-trigger_mail_2(getEmailMessage(), "DividendStocksFiltered_nimish.xlsx")
+trigger_mail_2(getEmailMessage(), file_name)
 
 print("---------- Python Code Successfully Ran ---------")
